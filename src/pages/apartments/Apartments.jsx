09f14e1ec20937019ac275efa7e129c3236fa184
Ali Bajwa <alibajwa6637@gmail.com>
2024-02-18 02:39:22 -0500

    data is connected using seprate data.js file

--- src/pages/apartments/Apartments.jsx
+++ src/pages/apartments/Apartments.jsx
@@ -1,6 +1,7 @@
 import "./apartments.css";
 import TopBanner from "../../components/topbanner/TopBanner";
 import Navbar from "../../components/navbar/Navbar";
+import { Appartments } from "../../Data.js";
 
 export default function Apartments() {
   return (
@@ -13,98 +14,21 @@ export default function Apartments() {
         <div className="apartments">
           <div className="apartmentsWrapper">
             <ul className="apartmentsList">
-              <li className="apartmentsListItem">
-                <img
-                  src="../../assets/appartments/appartment1.jpeg"
-                  alt=""
-                  className="apartmentImg"
-                />
-                <span className="apartmentDesc">
-                  {" "}
-                  <h4 className="DescTitle">
+              {/* we have use map function to repeat li component below for every elment in Appartments obj in data.js */}
+              {Appartments.map((u) => (
+                <li className="apartmentsListItem" key={u.id}>
+                  <img src={u.appartmentImg} alt="" className="apartmentImg" />
+                  <span className="apartmentDesc">
                     {" "}
-                    unit 302, 3243 Burligton ave, North york, Toronto{" "}
-                  </h4>
-                  <ul className="DescList">
-                    <li>Price: 355,900$</li>
-                    <li>Bathrooms:2</li>
-                    <li>Bedrooms:3 (contact for more details)</li>
-                  </ul>
-                </span>
-              </li>
-              <li className="apartmentsListItem">
-                <img
-                  src="../../assets/appartments/appartment2.jpeg"
-                  alt=""
-                  className="apartmentImg"
-                />
-                <span className="apartmentDesc">
-                  {" "}
-                  <h4 className="DescTitle">
-                    {" "}
-                    unit 2302, 4332 cresent Road, North york, Toronto
-                  </h4>
-                  <ul className="DescList">
-                    <li>Price = 253,400$</li>
-                    <li>Bathrooms: 1</li>
-                    <li>Bedrooms:2 (contact us for more details)</li>
-                  </ul>
-                </span>
-              </li>
-              <li className="apartmentsListItem">
-                <img
-                  src="../../assets/appartments/appartment3.jpeg"
-                  alt=""
-                  className="apartmentImg"
-                />
-                <span className="apartmentDesc">
-                  {" "}
-                  <h4 className="DescTitle">
-                    unit 1020, 345 Hallway street, downtown, Toronto
-                  </h4>
-                  <ul className="DescList">
-                    <li>Price: 744,500$</li>
-                    <li>Bedrooms:3</li>
-                    <li>Bathrooms:2 (contactus for more details)</li>
-                  </ul>
-                </span>
-              </li>
-              <li className="apartmentsListItem">
-                <img
-                  src="../../assets/appartments/appartment4.jpeg"
-                  alt=""
-                  className="apartmentImg"
-                />
-                <span className="apartmentDesc">
-                  {" "}
-                  <h4 className="DescTitle">
-                    unit: 901, 2019 Bessbrough drive, east york,Toronto
-                  </h4>
-                  <ul className="DescList">
-                    <li>price: 534,000$</li>
-                    <li>bedrooms: 2 </li>
-                    <li>Bathrooms: 1 (Contact us for more details)</li>
-                  </ul>
-                </span>
-              </li>
-              <li className="apartmentsListItem">
-                <img
-                  src="../../assets/appartments/appartment5.jpeg"
-                  alt=""
-                  className="apartmentImg"
-                />
-                <span className="apartmentDesc">
-                  {" "}
-                  <h4 className="DescTitle">
-                    unit: 405, 5434 Norway street, East york, Toronto
-                  </h4>
-                  <ul className="DescList">
-                    <li>Price: 325300$</li>
-                    <li>bedrooms: 3</li>
-                    <li>bathrooms:2 (Contact us for more details)</li>
-                  </ul>
-                </span>
-              </li>
+                    <h4 className="DescTitle">{u.address}</h4>
+                    <ul className="DescList">
+                      <li>{u.Price}</li>
+                      <li>Bathrooms: {u.bathrooms}</li>
+                      <li>Bedrooms:{u.Bedrooms} (contact for more details)</li>
+                    </ul>
+                  </span>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
--- src/pages/houses/Houses.jsx
+++ src/pages/houses/Houses.jsx
@@ -1,6 +1,7 @@
 import "./houses.css";
 import TopBanner from "../../components/topbanner/TopBanner";
 import Navbar from "../../components/navbar/Navbar";
+import { HousesData } from "../../Data.js";
 
 export default function Houses() {
   return (
@@ -13,166 +14,21 @@ export default function Houses() {
         <div className="houses">
           <div className="housesWrapper">
             <ul className="housesList">
-              <li className="housesListItem">
-                <img
-                  src="../../assets/houses/houseL1.jpeg"
-                  alt=""
-                  className="houseImg"
-                />
-                <span className="houseDesc">
-                  {" "}
-                  <h4 className="DescTitle"> 2324 Ramsy Road , Toronto</h4>
-                  <ul className="DescList">
-                    <li>Price: 475,900$</li>
-                    <li>Bathrooms:1</li>
-                    <li>Bedrooms:2 (contact for more details)</li>
-                  </ul>
-                </span>
-              </li>
-              <li className="housesListItem">
-                <img
-                  src="../../assets/houses/houseL2.jpeg"
-                  alt=""
-                  className="houseImg"
-                />
-                <span className="houseDesc">
-                  {" "}
-                  <h4 className="DescTitle">4953 Bannf Road, Toronto</h4>
-                  <ul className="DescList">
-                    <li>Price = 410,000$</li>
-                    <li>Bathrooms: 2</li>
-                    <li>Bedrooms:2 (contact us for more details)</li>
-                  </ul>
-                </span>
-              </li>
-              <li className="housesListItem">
-                <img
-                  src="../../assets/houses/houseL3.jpeg"
-                  alt=""
-                  className="houseImg"
-                />
-                <span className="houseDesc">
-                  {" "}
-                  <h4 className="DescTitle">3234 Bayview ave, Toronto</h4>
-                  <ul className="DescList">
-                    <li>Price: 644,500$</li>
-                    <li>Bedrooms:4</li>
-                    <li>Bathrooms:2 (contact us for more details)</li>
-                  </ul>
-                </span>
-              </li>
-              <li className="housesListItem">
-                <img
-                  src="../../assets/houses/houseL4.jpeg"
-                  alt=""
-                  className="houseImg"
-                />
-                <span className="houseDesc">
-                  {" "}
-                  <h4 className="DescTitle">456 Mount pleasent Road,Toronto</h4>
-                  <ul className="DescList">
-                    <li>price: 774,000$</li>
-                    <li>bedrooms: 4 </li>
-                    <li>Bathrooms: 2 (Contact us for more details)</li>
-                  </ul>
-                </span>
-              </li>
-              <li className="housesListItem">
-                <img
-                  src="../../assets/houses/houseL5.jpeg"
-                  alt=""
-                  className="houseImg"
-                />
-                <span className="houseDesc">
-                  {" "}
-                  <h4 className="DescTitle">312 Millwood road,Toronto</h4>
-                  <ul className="DescList">
-                    <li>Price: 502,300$</li>
-                    <li>bedrooms: 3</li>
-                    <li>bathrooms:2 (Contact us for more details)</li>
-                  </ul>
-                </span>
-              </li>
-              <li className="housesListItem">
-                <img
-                  src="../../assets/houses/houseL6.jpeg"
-                  alt=""
-                  className="houseImg"
-                />
-                <span className="houseDesc">
-                  {" "}
-                  <h4 className="DescTitle">432 pape ave , Toronto</h4>
-                  <ul className="DescList">
-                    <li>Price: 554,900$</li>
-                    <li>Bathrooms:1</li>
-                    <li>Bedrooms:2 (contact for more details)</li>
-                  </ul>
-                </span>
-              </li>
-              <li className="housesListItem">
-                <img
-                  src="../../assets/houses/houseL7.jpeg"
-                  alt=""
-                  className="houseImg"
-                />
-                <span className="houseDesc">
-                  {" "}
-                  <h4 className="DescTitle">2334 Eglinton ave East, Toronto</h4>
-                  <ul className="DescList">
-                    <li>Price = 734,000$</li>
-                    <li>Bathrooms: 2</li>
-                    <li>Bedrooms:4 (contact us for more details)</li>
-                  </ul>
-                </span>
-              </li>
-              <li className="housesListItem">
-                <img
-                  src="../../assets/houses/houseL8.jpg"
-                  alt=""
-                  className="houseImg"
-                />
-                <span className="houseDesc">
-                  {" "}
-                  <h4 className="DescTitle">342 Richard street, Toronto</h4>
-                  <ul className="DescList">
-                    <li>Price: 600,000$</li>
-                    <li>Bedrooms:3</li>
-                    <li>Bathrooms:2 (contactus for mmore details)</li>
-                  </ul>
-                </span>
-              </li>
-              <li className="housesListItem">
-                <img
-                  src="../../assets/houses/houseL9.jpg"
-                  alt=""
-                  className="houseImg"
-                />
-                <span className="houseDesc">
-                  {" "}
-                  <h4 className="DescTitle">436 Danfroth ave,Toronto</h4>
-                  <ul className="DescList">
-                    <li>price: 700,000$</li>
-                    <li>bedrooms: 4 </li>
-                    <li>Bathrooms: 2 (Contact us for more details)</li>
-                  </ul>
-                </span>
-              </li>
-              <li className="housesListItem">
-                <img
-                  src="../../assets/houses/houseL10.jpeg"
-                  alt=""
-                  className="houseImg"
-                />
-                <span className="houseDesc">
-                  {" "}
-                  <h4 className="DescTitle">3654 Lawrence ave,Toronto</h4>
-                  <ul className="DescList">
-                    <li>Price: 605,300$</li>
-                    <li>bedrooms: 4</li>
-                    <li>bathrooms:2 (Contact us for more details)</li>
-                  </ul>
-                </span>
-              </li>
+              {/* we have use map function to repeat li component below for every elment in HousesData obj in data.js */}
+              {HousesData.map((u) => (
+                <li className="housesListItem">
+                  <img src={u.houseImg} alt="" className="houseImg" />
+                  <span className="houseDesc">
+                    {" "}
+                    <h4 className="DescTitle"> {u.address}</h4>
+                    <ul className="DescList">
+                      <li>{u.Price}</li>
+                      <li>Bathrooms:{u.bathrooms}</li>
+                      <li>Bedrooms:{u.Bedrooms} (contact for more details)</li>
+                    </ul>
+                  </span>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
