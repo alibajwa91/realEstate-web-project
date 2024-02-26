6d9c3e99ed647611e4231bc628f96f097f49bb19
Ali Bajwa <alibajwa6637@gmail.com>
2024-02-26 13:10:04 -0500

    Google-map-api-has been added replace api key with your own, final intractive frontend

---
+++ src/components/googlemap/GoogleMap.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import "./googlemap.css";
+
+const GoogleMap = () => {
+  useEffect(() => {
+    // Paste the JavaScript code here
+
+    // This script loads the Google Maps JavaScript API.
+    // Make sure to replace 'YOUR_API_KEY' with your actual API key.
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=bjsbiedyedwiebywbdkahyiwdawubdusbjkca432&callback=initMap`;
+    script.defer = true;
+    document.head.appendChild(script);
+
+    // Define the initMap function to initialize the map
+    window.initMap = () => {
+      const markerPositions = [
+        { lat: 43.69441, lng: -79.37381 },
+        { lat: 43.70681, lng: -79.37098 },
+      ];
+      const map = new window.google.maps.Map(document.getElementById("map"), {
+        zoom: 9,
+        center: { lat: 43.65107, lng: -79.347015 },
+        mapId: "AMB_RealEstate",
+      });
+
+      markerPositions.forEach((position) => {
+        // Create a marker for each position
+        const marker = new window.google.maps.Marker({
+          position: position,
+          map: map,
+          title: "AMB-RealEstate Listings",
+        });
+      });
+    };
+  }, []);
+
+  return (
+    <div>
+      <h3>Map Of Available Properties</h3>
+      <div id="map"></div>
+    </div>
+  );
+};
+
+export default GoogleMap;
--- src/pages/sitemap/Sitemap.jsx
+++ src/pages/sitemap/Sitemap.jsx
@@ -1,17 +1,20 @@
 import "./sitemap.css";
 import TopBanner from "../../components/topbanner/TopBanner";
 import Navbar from "../../components/navbar/Navbar";
+import GoogleMap from "../../components/googlemap/GoogleMap";
 
 export default function Sitemap() {
   return (
     <div>
       <TopBanner />
-      <div className="NavAndHome">
+      <div className="NavAndMap">
         <div className="navBar">
           <Navbar />
         </div>
         <div className="sitemap">
-          <div className="sitemapWrapper"></div>
+          <div className="sitemapWrapper">
+            <GoogleMap />
+          </div>
         </div>
       </div>
     </div>
