744a07a79f3dddbb90791216f9deca85bf74fd36
Ali Bajwa <alibajwa6637@gmail.com>
2024-02-28 01:31:53 -0500

    error has been removed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import Apartments from "./pages/apartments/Apartments";
-import Contactus from "./pages/contactus/Contactus";
 import Home from "./pages/home/Home";
-import Houses from "./pages/houses/Houses";
-import SaleProperty from "./pages/saleProperty/SaleProperty";
 function App() {
-  return (
-    <Home />
-    // <SaleProperty />
-    // <Apartments />
-    // <Houses />
-    // <Contactus />
-  );
+  return <Home />;
 }
 
 export default App;
